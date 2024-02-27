836ce081d0a6433a5bace97559145843ec8e1f12
mdeandwibekti <mdeandwibekti@gmail.com>
2024-02-27 18:49:54 +0700

    Add project files.

---
+++ Pertemuan Ke1/Pertemuan Ke1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+
+	float njejari, nluas
+
+		cout << "masukan jejari = ";
+
+	cin >> njejari;
+
+	nluas = 3.14 * njejari * njejari;
+
+	cout << "luas lingkaran adalah = " << nluas << endl;
+
+	system("pause");
+
+}
+
